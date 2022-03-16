fd25c9f5dfab2f7744c943f372b339d2f7c34633
valeritromycin <valeritromycin@gmail.com>
2022-03-16 13:34:45 +0300

    added foreign key user to Post model

--- publication_app/models.py
+++ publication_app/models.py
@@ -3,11 +3,12 @@ from django.db import models
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, related_name='posts', on_delete=models.PROTECT)
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.PROTECT, null=True)
     create_date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=144, unique=False, blank=False, null=False)
     text = models.TextField(blank=False, null=False)
     image = models.ImageField(null=True, blank=True)
+    is_public = models.BooleanField(default=True)
 
 
 
