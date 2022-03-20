337e816864c56b3862e5353e9670d96aff25e31d
valeritromycin <valeritromycin@gmail.com>
2022-03-20 15:49:03 +0300

    added action to see tags and user to which current user is subscribed

--- subscription_app/api/views/subscriptions.py
+++ subscription_app/api/views/subscriptions.py
@@ -1,6 +1,9 @@
-from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin
+from rest_framework.decorators import action
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin, RetrieveModelMixin
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from publication_app.api.serializers.publications import PostSerializer
 from ..serializers.subscriptions import TagSubscriptionSerializer, UserSubscriptionSerializer
 from ...models import Subscription
 
@@ -15,6 +18,16 @@ class SubscribeTagView(GenericViewSet, CreateModelMixin, DestroyModelMixin):
     serializer_class = TagSubscriptionSerializer
 
 
-class SubscriptionsViewSet(GenericViewSet, ListModelMixin):
+class SubscriptionsViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     queryset = Subscription.objects.all()
     serializer_class = UserSubscriptionSerializer
+
+    @action(methods=["get", ], detail=False, name='subscription_users', serializer_class=UserSubscriptionSerializer)
+    def user_posts(self, request, pk=None):
+        instance = self.get_queryset().filter(subscription_user__isnull=False)
+        return Response(self.get_serializer(instance, many=True).data)
+
+    @action(methods=["get", ], detail=False, name='subscription_tags', serializer_class=TagSubscriptionSerializer)
+    def tag_posts(self, request, pk=None):
+        instance = self.get_queryset().filter(subscription_user__isnull=False)
+        return Response(self.get_serializer(instance, many=True).data)
