54ec042bdfced52c65c5a34b1acc23148f9b838e
valeritromycin <valeritromycin@gmail.com>
2022-03-20 11:53:29 +0300

    fixed likes working

--- publication_app/api/serializers/publications.py
+++ publication_app/api/serializers/publications.py
@@ -1,9 +1,8 @@
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from likes_app.api.serializers.likes import PostLikeSerializer
-from likes_app.services import is_fan
+from likes_app.models import Like
 from tags_app.api.serializers.tag import TagSerializer
 from ...models import Post
 
@@ -28,10 +27,12 @@ class PostSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
     likes = PostLikeSerializer(many=True, read_only=True)
 
+    @extend_schema_field(bool)
     def get_is_fan(self, obj) -> bool:
         user = self.context.get('request').user
-        return is_fan(obj, user)
+        likes = Like.objects.filter(user=user, post=obj.id)
+        return likes.exists()
 
     @extend_schema_field(int)
     def get_likes_count(self, instance) -> int:
-        instance.likes.count()
+        return instance.likes.count()
